30fb0490a267d494bc36b1dbeaf500ba5713180a
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2023-12-29 23:38:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = db.createClient(supabaseUrl, supabaseKey);
 
-import { PostHog } from 'posthog-node'
+const PostHog = require('posthog-node');
 
 const PostHogClient = new PostHog(
     process.env.POSTHOG,
